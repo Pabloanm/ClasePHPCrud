187df71f6d134ee2177573efb1a7547458829646
Pabloanm <pabloanm@yahoo.com>
2022-08-03 04:45:32 -0300

    Api EndPoints

---
+++ api/createCustomer.php
@@ -0,0 +1,21 @@
+<?php
+  //http://localhost/clasephpcrud/api/createCustomer.php?nombre=Pedro&apellido=Crespi&email=aasdasd@asdasd.com&telefono=234234&detalle=sdfsdf
+  $conexion = new mysqli("localhost", "root", "", "sistema") or die("not connected".mysqli_connect_error());
+  
+  if (isset($_GET['nombre'])) {
+    $nombre = $_GET['nombre'];
+    $apellido = $_GET['apellido'];
+    $email = $_GET['email'];
+    $telefono = $_GET['telefono'];
+    $detalle = $_GET['detalle'];
+
+    $sql = "INSERT INTO `clientes` (`id`, `nombre`, `apellido`, `email`, `telefono`, `detalle`) VALUES (NULL, '$nombre', '$apellido', '$email', '$telefono', '$detalle');";
+
+    $query = mysqli_query($conexion, $sql);
+    if($query){
+      echo"1 row inserted";
+    }else{
+      echo mysqli_error($conexion);
+    }
+  }
+?>
